53cef13cb32eee288f319e87bb5af128794424de
hamzaishtiaq0318-bit <hamzaishtiaq0318@gmail.com>
2026-09-21 15:51:27 +0500

    Update portfolio

--- test_glidely_dom.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-script = """
-setTimeout(() => {
-    console.log('GLIDELY_HTML:' + document.body.innerHTML.slice(0, 1000));
-}, 3000);
-"""
-
-# Let's run a test with CDP or playwright if available, or python script
